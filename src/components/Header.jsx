1d9122f5e03bf72d278dc1735e325e13bf8a1a29
Lina Aparicio <laparici9@gmail.com>
2024-07-23 22:26:09 -0500

    WIP responsive features and CV download

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
     <header className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <a
-                    className="logo flex items-center space-x-3 rtl:space-x-reverse font-bold text-2xl"
+                    className="logo flex items-center space-x-3 rtl:space-x-reverse font-bold text-2xl scroll-smooth"
                     href="#top"
                 >
                     Lina Aparicio
@@ -25,10 +25,10 @@ const Header = () => {
                     id="navbar-default"
                 >
                     <nav className="flex flex-col md:flex-row gap-x-10 gap-y-5 md:p-0 mt-4 p-4 border border-gray-100 bg-gray-200 md:bg-transparent opacity-80 md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 font-semibold text-lg ">
-                        <a href="#experiencia" className="hover:underline">Experiencia</a>
-                        <a href="#proyectos" className="hover:underline">Proyectos</a>
-                        <a href="#skills" className="hover:underline">Skills</a>
-                        <a href="#contacto" className="hover:underline">Contacto</a>
+                        <a href="#experiencia" className="hover:underline scroll-smooth">Experiencia</a>
+                        <a href="#proyectos" className="hover:underline scroll-smooth">Proyectos</a>
+                        <a href="#skills" className="hover:underline scroll-smooth">Skills</a>
+                        <a href="#contacto" className="hover:underline scroll-smooth">Contacto</a>
                     </nav>
                 </div>
             </div>
